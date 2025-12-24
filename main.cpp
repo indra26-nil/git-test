ec47ed34e9818e1b304fb0c81701c175c7ef4fec
indra26-nil <indranillaha4@gmail.com>
2025-12-24 09:58:18 +0530

    Adding new files and modified code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-
+    int n;
     cout << "PUSH" << endl;
     cout << "Successfully" << endl ;
+    cout << "Enter number : ";
+    cin >> n;
+    cout << "Your number is " <<n;
     return 0;
 }
